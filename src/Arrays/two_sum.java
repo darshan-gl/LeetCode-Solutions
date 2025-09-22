6c28109ba6ad2edd8332eaee61cc98afe02aa50a
darshan-gl <gldarshann@gmail.com>
2025-09-22 08:58:18 +0530

    Update two_sum.java

--- src/Arrays/two_sum.java
+++ src/Arrays/two_sum.java
@@ -15,18 +15,13 @@ public class two_sum{
         int[] result=twoSum(nums,target);
         System.out.println(Arrays.toString(result));
     }
-
-    static int[] twoSum(int[] nums,int target)
-    {
-        HashMap<Integer,Integer> hm=new HashMap<>();
-    
-        for(int i=0;i<nums.length;i++){
-            Integer index=hm.get(nums[i]);
-           
-            if(index!=null)
-            return new int[]{index,i};
-             
-            hm.put(target-nums[i],i);
+  static int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++) {
+            for(int j=i+1;j<nums.length;j++) {
+                if(target == nums[i] + nums[j]) {
+                    return new int[]{i, j};
+                }
+            }
         }
         return new int[]{};
     }
