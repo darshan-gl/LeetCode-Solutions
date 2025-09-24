3df9b959753d2439f1412a6f6761f70f399a86ba
darshan-gl <gldarshann@gmail.com>
2025-09-25 00:35:29 +0530

    #35

---
+++ src/Arrays/Firstandlastposition.java
@@ -0,0 +1,31 @@
+package Arrays;
+public class Firstandlastposition {
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+        int[] nums = {5,7,7,8,8,10};
+        int target = 8;
+
+        int[] ans = obj.searchRange(nums, target);
+
+        System.out.println("[" + ans[0] + ", " + ans[1] + "]");
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int[] res = {-1, -1};
+
+        for (int i=0;i<nums.length;i++) {
+            if (nums[i]==target) {
+                res[0]=i;
+                break;
+            }
+        }
+
+        for (int j=nums.length-1;j>=0;j--) {
+            if (nums[j]==target) {
+                res[1]=j;
+                break;
+            }
+        }
+        return res;
+    }
+}
