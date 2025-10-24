91d777ef00124fa3d87368fc861e1d2b47af98af
darshan-gl <gldarshann@gmail.com>
2025-10-24 10:40:45 +0530

    #leetcode58

---
+++ src/Strings/LengthOfLastWord.java
@@ -0,0 +1,21 @@
+package Strings;
+//LeetCode 58: Length of Last Word
+public class LengthOfLastWord {
+ public static void main(String[] args) {
+     String s = "Hello World";
+     System.out.println(lengthOfLastWord(s)); // Output: 5
+ }
+
+ public static int lengthOfLastWord(String s) {
+     int count = 0;
+     String str = s.trim();
+     for (int i = str.length() - 1; i >= 0; i--) {
+         if (str.charAt(i) != ' ') {
+             count++;
+         } else {
+             break;
+         }
+     }
+     return count;
+ }
+}
