d6bac0c21598ca369139602fec917005a0d7af5b
darshan-gl <gldarshann@gmail.com>
2025-11-03 21:49:10 +0530

    leetcode119

---
+++ src/Arrays/PascalTraingle.java
@@ -0,0 +1,20 @@
+package Arrays;
+import java.util.*;
+
+public class PascalTraingle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res=new ArrayList<>();
+        
+        for(int i=0;i<numRows;i++){
+            List<Integer> row=new ArrayList<>();
+            for(int j=0;j<=i;j++)
+            {
+                if(j==0||j==i) 
+                row.add(1);
+                else row.add(res.get(i-1).get(j-1)+res.get(i-1).get(j));
+            }
+            res.add(row);
+        }
+        return res;
+    }
+}
